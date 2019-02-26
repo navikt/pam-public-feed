5ce495e8b104e6c169c04046082e21787ef72c63
eziztm <eziztm@yahoo.com>
2019-02-26 14:37:39 +0100

    added spring boot stuff

---
+++ src/main/java/no/nav/pam/feed/Application.java
@@ -0,0 +1,13 @@
+package no.nav.pam.feed;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
