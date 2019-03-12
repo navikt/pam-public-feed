ae209faf705a3a2ec4964a6c9aff981aa5656e2c
eziztm <eziztm@yahoo.com>
2019-03-12 13:49:42 +0100

    added pagination feature

--- src/main/kotlin/no/nav/pam/feed/ad/SearchClient.kt
+++ src/main/kotlin/no/nav/pam/feed/ad/SearchClient.kt
@@ -1,14 +1,23 @@
 package no.nav.pam.feed.ad
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.ktor.application.call
 import io.ktor.client.HttpClient
 import io.ktor.client.call.call
 import io.ktor.client.engine.apache.Apache
+import io.ktor.client.request.url
 import io.ktor.client.response.readBytes
+import io.ktor.content.TextContent
+import io.ktor.http.ContentType
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import kotlinx.coroutines.async
+
+
+data class FeedAd(val title: String)
 
 fun Routing.feed(
         searchApiHost: String = "https://pam-search-api.nais.oera-q.local",
@@ -16,12 +25,35 @@ fun Routing.feed(
 ) {
     get("/api/feed") {
         clientFactory().use {
-            val searchRequest = async { it.call("$searchApiHost/ad/_search").response.readBytes() }
+            try {
+                val from = call.parameters["from"]?.toInt() ?: 0
+                val size = call.parameters["size"]?.toInt() ?: 1
+                val requestBody = """{
+                "sort": [{"published": "desc"}],
+                "query": {
+                   "term": {"status": "ACTIVE"}},
+                 "_source": {
+                   "includes": [  ],
+                   "excludes": [ ]
+                 },
+                 "from": $from,
+                 "size": $size
+                 }""".trimIndent()
 
-            // TODO conversion
-            
-            call.respond(searchRequest.await())
+                val searchRequest = async {
+                    it.call {
+                        method = HttpMethod.Post
+                        url("$searchApiHost/ad/_search")
+                        body = TextContent(text = requestBody, contentType = ContentType.Application.Json)
+                    }.response.readBytes()
+                }
+
+                // TODO conversion
+
+                call.respond(searchRequest.await())
+            } catch (e: NumberFormatException) {
+                call.respond(HttpStatusCode.BadRequest, "One of parameters has wrong format")
+            }
         }
-
     }
 }
