1d60b1d6bb768cf98d080da4563ce21053a33491
eziztm <eziztm@yahoo.com>
2019-04-03 10:17:40 +0200

    removed unnecessary json annotations

--- src/main/kotlin/no/nav/pam/feed/ad/OutputPayload.kt
+++ src/main/kotlin/no/nav/pam/feed/ad/OutputPayload.kt
@@ -1,13 +1,12 @@
 package no.nav.pam.feed.ad
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import no.nav.pam.feed.Environment
 import java.time.ZonedDateTime
 
 val env: Environment = Environment()
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+
 data class FeedRoot(val content: List<FeedAd>,
                     val totalElements: Int,
                     val number: Int,
@@ -17,7 +16,6 @@ data class FeedRoot(val content: List<FeedAd>,
     val last: Boolean = number == totalPages
 }
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 //@JsonInclude(JsonInclude.Include.NON_NULL)
 data class FeedAd(val uuid: String,
                   val published: ZonedDateTime,
@@ -32,7 +30,6 @@ data class FeedAd(val uuid: String,
                   val occupations: List<String>,
                   val link: String)
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class FeedLocation(val country: String,
                         val address: String?,
