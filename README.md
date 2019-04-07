5628e769a1694ff7c34cfac2d9e9ac2814421de8
Øyvind Stegard <oyvind.stegard@nav.no>
2019-04-07 20:34:07 +0200

    [PAM-2942] Add internal endpoint for getting info about an existing token
    Update README.md with instructions on usage.

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Job ads JSON feed
 
 ## Authentication
 
-The feed API requires authentication using the `Authorization` header, type `Bearer` and a signed JWT token.
+The feed API requires authentication using the `Authorization` header, type
+`Bearer` and a signed JWT token.
 
 ## Create new API token
 
@@ -12,8 +13,25 @@ The feed API requires authentication using the `Authorization` header, type `Bea
 
         curl https://pam-public-feed.nais.oera.no/public-feed/internal/newApiToken -d subject=EMAIL
     
-    where EMAIL is the email address provided as customer contact information.
+    where `EMAIL` is the email address provided as customer contact information.
     
-2. Log/register generated token and email.
+2. Log/register generated token and email (Confluence page).
 
-3. Supply customer with information about the `Authorization` header value provided in step 2.
+3. Supply customer with information about the `Authorization` header value from
+   output in step 2.
+
+## Get informaation about an existing token
+
+Call:
+
+    curl -H 'Authorization: Bearer TOKEN' https://pam-public-feed.nais.oera.no/public-feed/internal/apiTokenInfo
+
+where `TOKEN` is the encoded token value. The endpoint decodes and verifies the
+token, then responds:
+
+    Token information:
+    Algorithm:    HS256
+    Subject:      contact@some-customer.com
+    Issuer:       nav.no
+    Issued at:    Sun Apr 07 20:28:18 CEST 2019
+    Verification: OK
--- src/main/kotlin/no/nav/pam/feed/auth/AuthSupport.kt
+++ src/main/kotlin/no/nav/pam/feed/auth/AuthSupport.kt
@@ -3,12 +3,16 @@ package no.nav.pam.feed.auth
 import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.exceptions.JWTDecodeException
 import io.ktor.application.call
+import io.ktor.auth.HttpAuthHeader
+import io.ktor.auth.parseAuthorizationHeader
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.receiveParameters
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Route
+import io.ktor.routing.get
 import io.ktor.routing.post
 import mu.KotlinLogging
 import java.util.*
@@ -37,6 +41,30 @@ class JwtTokenFactory(issuer: String, audience: String, secret: String) {
 }
 
 fun Route.tokenManagementApi (tokenFactory: JwtTokenFactory) {
+
+    get("apiTokenInfo") {
+        val authHeader = call.request.parseAuthorizationHeader()
+        if (authHeader != null && authHeader is HttpAuthHeader.Single && authHeader.authScheme == "Bearer") {
+            try {
+                val decoded = JWT.decode(authHeader.blob)
+                val valid = try { tokenFactory.newHmacJwtVerifier().verify(decoded); "OK" } catch (e: Exception) { "FAIL: ${e.message}" }
+                call.respondText("""
+
+                    Token information:
+                    Algorithm:    ${decoded.algorithm}
+                    Subject:      ${decoded.subject}
+                    Issuer:       ${decoded.issuer}
+                    Issued at:    ${decoded.issuedAt}
+                    Verification: ${valid}
+
+                """.trimIndent())
+            } catch (e: JWTDecodeException) {
+                call.respondText("Unable to decode JWT token: ${e.message}\n")
+            }
+        } else {
+            call.respondText("Authorization header not present or type is unsupported. Supply token in Authorization header.\n")
+        }
+    }
 
     post("newApiToken") {
         try {
