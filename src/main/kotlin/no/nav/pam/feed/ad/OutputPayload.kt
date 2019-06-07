ea56d325186cc1ee88be544e1805f8b72c37105a
Einar Bjerve <einar.bjerve@nav.no>
2019-06-07 07:24:47 +0200

    Endret ihht kommentarer. Alignet constructor-parametere i OutputPayload

--- src/main/kotlin/no/nav/pam/feed/ad/OutputPayload.kt
+++ src/main/kotlin/no/nav/pam/feed/ad/OutputPayload.kt
@@ -3,10 +3,11 @@ package no.nav.pam.feed.ad
 import com.fasterxml.jackson.annotation.JsonInclude
 import java.time.ZonedDateTime
 
-data class FeedRoot(val content: List<FeedAd>,
-                    val totalElements: Int,
-                    val pageNumber: Int,
-                    val pageSize: Int) {
+data class FeedRoot(
+        val content: List<FeedAd>,
+        val totalElements: Int,
+        val pageNumber: Int = 0,
+        val pageSize: Int) {
     val totalPages: Int = totalElements / pageSize + if (totalElements % pageSize > 0) 1 else 0
     val first: Boolean = pageNumber == 0
     val last: Boolean = pageNumber >= totalPages - 1
@@ -14,27 +15,30 @@ data class FeedRoot(val content: List<FeedAd>,
 }
 
 //@JsonInclude(JsonInclude.Include.NON_NULL)
-data class FeedAd(val uuid: String,
-                  val published: ZonedDateTime,
-                  val expires: ZonedDateTime,
-                  val workLocations: List<FeedLocation>,
-                  val title: String,
-                  val description: String?,
-                  val source: String?,
-                  val applicationDue: String?,
-                  val occupations: List<String>,
-                  val link: String,
-                  val employer: FeedEmployer)
+data class FeedAd(
+        val uuid: String,
+        val published: ZonedDateTime,
+        val expires: ZonedDateTime,
+        val workLocations: List<FeedLocation>,
+        val title: String,
+        val description: String?,
+        val source: String?,
+        val applicationDue: String?,
+        val occupations: List<String>,
+        val link: String,
+        val employer: FeedEmployer)
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-data class FeedLocation(val country: String,
-                        val address: String?,
-                        val city: String?,
-                        val postalCode: String?,
-                        val county: String?,
-                        val municipal: String?)
+data class FeedLocation(
+        val country: String,
+        val address: String?,
+        val city: String?,
+        val postalCode: String?,
+        val county: String?,
+        val municipal: String?)
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-data class FeedEmployer(val name: String,
-                        val orgnr: String?,
-                        val description: String?)
+data class FeedEmployer(
+        val name: String,
+        val orgnr: String?,
+        val description: String?)
