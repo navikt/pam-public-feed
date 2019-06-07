af91d3f8660c5e32b91033f6ab3d87c1d4ebb9b3
Einar Bjerve <einar.bjerve@nav.no>
2019-06-07 14:35:52 +0200

    Revert "Added possibility to filter the feed due to consumer needs. Probably need to include some way to specify how to match (greater/less than, negation) before all types are useful. Added descending sort by updated"
    This reverts commit 94f68648ac293ba7572368acbb699e17fd8f9a7b.

--- src/main/kotlin/no/nav/pam/feed/Bootstrap.kt
+++ src/main/kotlin/no/nav/pam/feed/Bootstrap.kt
@@ -1,6 +1,8 @@
 package no.nav.pam.feed
 
 import apiDoc
+import com.fasterxml.jackson.core.util.DefaultIndenter
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
@@ -17,7 +19,6 @@ import io.ktor.client.features.json.JsonFeature
 import io.ktor.features.CORS
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
-import io.ktor.features.StatusPages
 import io.ktor.http.HttpHeaders
 import io.ktor.jackson.jackson
 import io.ktor.routing.route
@@ -86,9 +87,6 @@ fun searchApi(
             allowCredentials = true
             header("Authorization")
             anyHost()
-        }
-        install(StatusPages) {
-            feed()
         }
         routing {
 
